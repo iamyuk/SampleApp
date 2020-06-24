a0afe14d6e28eb082963eba0ef4a993b24d2d441
yyu8k <yyu8k@icloud.com>
2020-06-24 20:42:51 +0900

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@Autowired
+	MessageService messageService;
+
+	@RequestMapping("/")
+	public String index (ModelMap modelMap) {
+		List<Message> messages = messageService.findAll();
+		modelMap.addAttribute("messages", messages);
+		modelMap.addAttribute("message", "Hello world");
+		return "index";
+	}
+
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public String create(@ModelAttribute MessageForm messageForm, ModelMap modelMap) {
+		Message message = new Message();
+		message.name = messageForm.getName();
+		message.message = messageForm.getMessage();
+		messageService.save(message);
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "messages/{id}", method = RequestMethod.POST)
+	public String destroy(@PathVariable("id") Integer id) {
+		messageService.delete(id);
+		return "redirect:/";
+	}
+
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="messages")
+public class Message {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer id;
+	public String name;
+	public String message;
+}
